a094ffad1673d3bdb88915a2e93b2fcc4a4c1bd0
iamasianman <kevinle1991@gmail.com>
2020-08-18 20:05:08 -0700

    implemented a list or recipes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import RecipeList from './RecipeList';
 
 function App() {
-  return null;
+  const sampleRecipes = [
+    {
+      id: 1,
+      name: 'Plain Chicken',
+      servings: 3,
+      cookingTime: '1:45',
+      instructions: '1. blah\n 2. blah? blah?'
+    },
+    {
+      id: 2,
+      name: 'Pizza',
+      servings: 5,
+      cookingTime: '2:00',
+      instructions: '1. make pizza\n 2. blah? blah?'
+    }
+  ]
+  return (
+    <>
+      <RecipeList recipes={sampleRecipes} />
+    </>
+  )
 }
 
 export default App;
