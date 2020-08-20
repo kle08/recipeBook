e0e3e961385d9915c36fbd1dedb36ded2d2dcf05
iamasianman <kevinle1991@gmail.com>
2020-08-19 22:29:04 -0700

    implemented delete and add button

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,14 +2,18 @@ import React from 'react'
 import IngredientList from './IngredientsList';
 
 export default function Recipe(props) {
-  const { name, cookingTime, servings, instructions, ingredients } = props
+  const { id, name, cookingTime, servings, instructions, ingredients, handleRecipeDelete } = props
   return (
     <div className='recipe'>
       <div className='recipe__header'>
         <h3 className='recipe__title'>{name}</h3>
         <div>
           <button className='btn btn--primary mr-1'>Edit</button>
-          <button className='btn btn--danger'>Delete</button>
+          <button
+            className='btn btn--danger'
+            onClick={() => handleRecipeDelete(id)}
+          >
+            Delete</button>
         </div>
       </div>
       <div className='recipe__row'>
